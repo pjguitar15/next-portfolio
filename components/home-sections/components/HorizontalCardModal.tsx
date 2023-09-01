3e19e1f301518ec116901632025ba08df61d6489
Philcob Suzuki Josol <pjguitar15@gmail.com>
2023-09-01 08:41:13 +0800

    modal now displays more description. it also displays conditionally depending on which modal is clicked.

--- components/home-sections/components/HorizontalCardModal.tsx
+++ components/home-sections/components/HorizontalCardModal.tsx
@@ -1,12 +1,26 @@
 'use client'
+import { useState, useEffect } from 'react'
 import { useGlobalContext } from '@/context/ModalContext'
+import { CardData } from '../../../data/HorizontalCardsData'
 
 const HorizontalCardModal = () => {
+  const [moreDescription, setMoreDescription] = useState('test')
   const { isModalOpen, setIsModalOpen, modalTitle, modalDescription } =
     useGlobalContext()
   const handleClose = () => {
     setIsModalOpen(!isModalOpen)
   }
+
+  useEffect(() => {
+    console.log(CardData)
+    CardData.forEach((item) => {
+      console.log(item)
+      if (item.title === modalTitle) {
+        setMoreDescription(item.description)
+      }
+      return ''
+    })
+  }, [modalTitle])
   return (
     <div
       className={`fixed inset-0 ${
@@ -22,11 +36,12 @@ const HorizontalCardModal = () => {
           </div>
           <div>
             <p className='text-white'>{modalDescription}</p>
+            <p className='text-white mt-5 leading-7'>{moreDescription}</p>
             <button
               onClick={handleClose}
-              className='mt-5 bg-light-green text-black px-4 py-2 rounded hover:bg-green-400'
+              className='mt-5 bg-light-green text-black px-4 py-2 rounded font-medium hover:bg-green-400'
             >
-              Close
+              Back to home
             </button>
           </div>
         </div>
